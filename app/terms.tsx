a11b4f058b81e9ac8b79792dc8cdf92920e0cc20
Alexandre1-boop <lbcmess@gmail.com>
2025-07-08 20:51:30 +0200

    Delete app directory

--- app/terms.tsx
+++
@@ -1,8 +0,0 @@
-export default function TermsPage() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Conditions Générales d'Utilisation</h1>
-      <p>Bienvenue sur Foi_biblique. En utilisant ce site, vous acceptez les présentes conditions.</p>
-    </div>
-  );
-}
