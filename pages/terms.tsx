39d632e1fe7f2514524847e0c2a8324ab21379df
Alexandre1-boop <lbcmess@gmail.com>
2025-07-08 16:32:07 +0200

    Create terms.tsx

---
+++ pages/terms.tsx
@@ -0,0 +1,8 @@
+export default function TermsPage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Conditions Générales d'Utilisation</h1>
+      <p>Bienvenue sur Foi_biblique. En utilisant ce site, vous acceptez les présentes conditions.</p>
+    </div>
+  );
+}
