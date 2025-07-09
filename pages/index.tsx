5e5aa2c0b832c32baa667f88a480b86df6025aa1
Alexandre1-boop <lbcmess@gmail.com>
2025-07-09 17:38:22 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,15 @@ export default function Home() {
     <main style={{ padding: '2rem' }}>
       <h1>Foi Biblique</h1>
       <p>Un verset par jour pour raviver ta foi ✝️</p>
+      <button style={{ padding: "10px", fontSize: "18px", backgroundColor: "#010101", color: "white", borderRadius: "5px" }}>
+  Se connecter avec TikTok
+      </button>
+
+
+
+
+
+      
     </main>
   );
 }
